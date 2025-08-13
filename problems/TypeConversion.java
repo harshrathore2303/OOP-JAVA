aab61b3a7f4a882726a0379ba52d4337e6192536
Harsh Rathore <harsh.rathore.2303@gmail.com>
2025-08-13 21:07:12 +0530

    automatic type converions

---
+++ problems/TypeConversion.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        // Automative type conversion can happen from small to large primitive datatypes
+        // byte → short → int → long → float → double
+
+        int small = 6;
+        double large = small; // same for float and long also
+        System.out.println(large);
+        
+        // No automatic conversions for wrapper classes
+        Integer s = 7;
+        Double d = s; // this will give compile time error
+
+    }
+}
